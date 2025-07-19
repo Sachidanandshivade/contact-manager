190c248f4045afd138003b6938b4c3c701438ac6
Sachidanand <sachidanandshivade948@gmail.com>
2025-07-19 19:01:34 +0530

    search by name or mail

--- src/App.js
+++ src/App.js
@@ -5,21 +5,28 @@ import { v4 as uuidv4 } from "uuid";
 
 function App(){
   const[contacts,setContacts]=useState([]);
-
-  const addContacts = (contact) => {
-  setContacts([...contacts, { id: uuidv4(), ...contact }]);
-};
+  const[searchterm,setSearchterm]=useState('');
+  const addContacts=(contact)=>{
+    setContacts([...contacts,contact]);
+  };
 
   const deleteContact=(id)=>{
     const updated =contacts.filter((c)=>c.id !== id);
     setContacts(updated);
   };
 
+  const filter=contacts.filter(contact=>
+    contact.name.toLowerCase().includes(searchterm.toLowerCase())||
+    contact.email.toLowerCase().includes(searchterm.toLowerCase())
+  );
+
   return(
     <div className="App">
     <h1>Contact manager</h1>
     <ContactForm addContact={addContacts} />
-    <ContactList contacts={contacts} onDelete={deleteContact}/>
+    <input type="text" placeholder="search by name or email" value={searchterm}
+    onChange={(e)=>setSearchterm(e.target.value)} />
+    <ContactList contacts={filter} onDelete={deleteContact}/>
     </div>
   );
 }
